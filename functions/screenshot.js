0a3b2d8c863d1bef59d9758c81a4cbe8951239c4
Art Rosnovsky <art.rosnovsky@auth0.com>
2020-05-14 17:06:59 -0700

    some more screenshot tweaking

--- functions/screenshot.js
+++ functions/screenshot.js
@@ -20,14 +20,15 @@ exports.handler = async () => {
   docs.result.forEach(async doc => {
     const SITE_URL = doc.url;
 
-    const url = `https://api.microlink.io??url=${SITE_URL}&waitFor=5&screenshot=true&meta=true&overlay.browser=light&overlay.background=%23bebebe&embed=screenshot.url`
+    const url = `https://api.microlink.io?url=${SITE_URL}&waitFor=5&screenshot=true&meta=true&overlay.browser=light&overlay.background=%23cecece&embed=screenshot.url`
 
     const fetchScreenshot = await fetch(url)
     let screenshotImage = await fetchScreenshot.arrayBuffer()
     let buff = await new Buffer.from(new Uint8Array(await screenshotImage)); 
     client.assets.upload('image', buff, {
       filename: `${doc.appId}-screenshot.png`
-      }).then(imageAsset => {
+      })
+      .then(imageAsset => {
         const mutations = [{
           patch: {
             id: doc._id,
@@ -54,5 +55,6 @@ exports.handler = async () => {
           .then(result => {console.log(result); return ({"body": JSON.stringify(result), "statusCode": "200"})})
           .catch(error => new Error(error))
       })
+    return { "statusCode": "200" }
   })
 }
