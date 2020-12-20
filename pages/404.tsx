f2a27cdf4ac5bff6077d1e1b437c8fbcdd1f8865
Artem Rosnovsky <inbox@podtema.com>
2020-12-20 01:04:46 -0800

    404 init

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
+
+const PageNotFound = () => {
+  return (
+    <>
+      <Nav />
+      
+      <Footer />
+    </>
+  );
+};
+
+export default PageNotFound;
